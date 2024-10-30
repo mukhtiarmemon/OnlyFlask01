33fee1621bd388872565e8d0e3c846ab2602084e
mukhtiarmemon <mukhtiar001@yahoo.com>
2024-10-30 17:32:07 +0500

    removed je

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ def home():
     return "Hello, Again with Flask Public Apppp!"
 
 if __name__ == "__main__":
-    # Set the host to '0.0.0.0' to make it accessible externally, or use '127.0.0.1' for local access only
+    # Set the host to '0.0.0.0' to make it accessible externally
+    # or use '127.0.0.1' for local access only
     # Specify port 8080 (or any other preferred port)
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=9999)
