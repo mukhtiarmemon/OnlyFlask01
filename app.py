aec80adcfadcf6bc4313afb2eef76e0fb9a02e6f
mukhtiarmemon <mukhtiar001@yahoo.com>
2024-10-30 16:06:58 +0500

    pp

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Again with Flask Public App!"
+    return "Hello, Again with Flask Public Apppp!"
 
 if __name__ == "__main__":
     # Set the host to '0.0.0.0' to make it accessible externally, or use '127.0.0.1' for local access only
